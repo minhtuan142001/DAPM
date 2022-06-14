b2289ed904df24375bb6caafde28591ce0a37023
tuan020501 <minhtuan25200001@gmail.com>
2022-06-14 20:13:59 +0700

    push

---
+++ public/view/header.php
@@ -0,0 +1,34 @@
+<header>
+    <div class="container">
+        <div class="header">
+            <div class="header__left">
+                <h3><a href="/">CỔNG TIÊM CHỦNG VACCINE CHO TRẺ EM</a></h3>
+            </div>
+            <div class="header__middle">
+                <div class="header__searchbox">
+                    <form action="/tim-kiem" method="">
+                        <input type="text" placeholder='Bạn muốn tìm kiếm gì?'>
+                    </form>
+                </div>
+                <ul class="header__menu">
+                    <li>Trang Chủ</li>
+                    <li>Thông Báo</li>
+                    <li>Đăng Ký Tiêm Chủng</li>
+                    <li><a href="/thong-ke">Thống Kê</a></li>
+                </ul>
+            </div>
+            <div class="header__right">
+                <ul class="header__contact">
+                    <li>Đường dây nóng: 0xxxxxxx</li>
+                    <li>Đường dây nóng: 0xxxxxxx</li>
+                    <li>Đường dây nóng: 0xxxxxxx</li>
+                </ul>
+                <div class="header__btn-group">
+                    <button> Đăng Nhập </button>
+                    <span>/</span>
+                    <button> Đăng Ký </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
